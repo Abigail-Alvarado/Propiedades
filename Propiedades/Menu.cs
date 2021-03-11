34327ae0d9180f8eda599b2f966dc69c0fb36c9d
Abigail Gaitan <abi49386307@outlook.com>
2021-03-10 23:50:48 -0600

    Diseños

---
+++ Propiedades/Menu.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Propiedades
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dueños vclientes = new Dueños();
+            vclientes.Show();
+            this.SetVisibleCore(false);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            propiedades vclientes = new propiedades();
+            vclientes.Show();
+            this.SetVisibleCore(false);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            datos vclientes = new datos();
+            vclientes.Show();
+            this.SetVisibleCore(false);
+
+        }
+    }
+}
--- Propiedades/datos.cs
+++ Propiedades/datos.cs
@@ -10,16 +10,11 @@ using System.Windows.Forms;
 
 namespace Propiedades
 {
-    public partial class Form1 : Form
+    public partial class datos : Form
     {
-        public Form1()
+        public datos()
         {
             InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
         }
     }
 }
